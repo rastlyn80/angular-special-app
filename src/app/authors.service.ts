2d1749411b88994e31cd123dbd9f08ec1680bf5a
Gabriele Baldocci <gabrielebaldocci@gabrielebaldocci.com>
2017-10-07 01:43:22 +0100

    Build a component, service and connected them to display a list of authors. Also made good use of ngFor.

---
+++ src/app/authors.service.ts
@@ -0,0 +1,5 @@
+export class AuthorsService {
+  getAuthors() {
+    return ["Borges", "Cervantes", "Allende", "Ciassani"];
+  }
+}
---
+++ src/app/authors/authors.component.ts
@@ -0,0 +1,18 @@
+import { AuthorsService } from './../authors.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'authors',
+  templateUrl: './authors.component.html',
+})
+export class AuthorsComponent implements OnInit {
+  authors;
+
+  constructor(service: AuthorsService) { 
+    this.authors = service.getAuthors();
+  }
+
+  ngOnInit() {
+  }
+
+}
